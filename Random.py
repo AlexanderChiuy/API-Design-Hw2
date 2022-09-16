8c41e425e3e107afe2dbf9bd126c475456724121
Alexander Chiu <achiu2@andrew.cmu.edu>
2022-09-16 15:14:58 -0400

    initial commit

---
+++ Random.py
@@ -0,0 +1,18 @@
+
+
+def selectRandom(choice):
+  '''
+  A function that will return a random item from the inputted "choices" list.
+
+  Args:
+   - choices: a list of possible items to choose from. This list can compose of 
+              different typed items or all the same types. Ex. choices = ["a", 1, "b", dict()] or ["hello", "hi", "howdy"]
+              Will raise a ValueError for None or empty list.
+
+  Returns: an item randomly from the choices list. Return type can be any type in the choices list
+
+  Example Use Case: 
+     randomColor = selectRandom(["red", "yellow", "blue"])
+  '''
+  pass
+  
