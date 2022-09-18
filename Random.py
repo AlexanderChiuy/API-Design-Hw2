0ba2165bfe0d9fc0f61a5c99767ec58ded43b31f
Alexander Chiu <achiu2@andrew.cmu.edu>
2022-09-18 12:22:04 -0400

    made final changes to documentation

--- Random.py
+++ Random.py
@@ -6,10 +6,13 @@ def selectRandom(choice):
 
   Args:
    - choices: a list of possible items to choose from. This list can compose of 
-              different typed items or all the same types. Ex. choices = ["a", 1, "b", dict()] or ["hello", "hi", "howdy"]
-              Will raise a ValueError for None or empty list.
+              different typed items or all the same type. 
+              Example: 
+                  choices = ["a", 1, "b", dict()] or ["hello", "hi", "howdy"]
+              Exception:
+                  ValueError for None or empty list 
 
-  Returns: an item randomly from the choices list. Return type can be any type in the choices list
+  Returns: an item randomly from the "choices" list. Return type can be any type in the "choices" list
 
   Example Use Case: 
      randomColor = selectRandom(["red", "yellow", "blue"])
